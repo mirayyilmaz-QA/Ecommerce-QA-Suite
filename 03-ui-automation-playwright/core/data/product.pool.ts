e17e7a3bc6a055a09a8854970472d830329f6296
mirayyilmaz-QA <mirayylmaz@gmail.com>
2026-01-29 19:09:11 +0300

    feat: successfully move playwright project to 03-automation folder

---
+++ 03-ui-automation-playwright/core/data/product.pool.ts
@@ -0,0 +1,18 @@
+
+export const productPool = [
+    {
+        name: 'Radiant Tee',
+        size: 'M',
+        color: 'Blue'
+    },
+    {
+        name: 'Breathe-Easy Tank',
+        size: 'S',
+        color: 'Purple'
+    },
+    {
+        name: 'Argus All-Weather Tank',
+        size: 'L',
+        color: 'Gray'
+    }
+];

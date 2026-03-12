e2bf81d774ed13af98599e790483795776cb7e4f
mirayyilmaz-QA <mirayylmaz@gmail.com>
2026-03-12 22:25:49 +0300

    updated workflows-reports

--- 04-api-testing/playwright-api/playwright.config.ts
+++ 04-api-testing/playwright-api/playwright.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
 
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['list']
+  ],
+
   use: {
     baseURL: 'https://dummyjson.com'
   },
